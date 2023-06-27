ebc86e147a81747213bd493155619ced6bfa8c4a
Vlozo <alevelozo26@gmail.com>
2023-06-27 11:54:29 -0300

    Atualizando CRUD

--- hora_escola/core/views.py
+++ hora_escola/core/views.py
@@ -2,6 +2,5 @@ from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse
 
-# Create your views here.
 def home(request):
     return HttpResponse("Hello World!")
