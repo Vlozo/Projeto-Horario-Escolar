5f15047ffa6fc5050227340dd100f1697be6a5e4
Vlozo <alevelozo26@gmail.com>
2023-06-27 11:54:29 -0300

    Atualizando CRUD

--- hora_escola/core/admin.py
+++ hora_escola/core/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import *
 
+class TurmaAdmin(admin.ModelAdmin):
+    list_display = ("turma",)
+
+class AlunoAdmin(admin.ModelAdmin):
+    list_display = ("nome", "turma",)
+    list_filter = ("turma",)
+    search_fields = ("nome",)
+
+class ResponsavelAdmin(admin.ModelAdmin):
+    list_display = ("nome", "telefone", "dependente",)
+    search_fields = ("nome",)
+
+admin.site.register(Turma, TurmaAdmin)
+admin.site.register(Aluno, AlunoAdmin)
+admin.site.register(Responsavel, ResponsavelAdmin)
 # Register your models here.
--- hora_escola/urls.py
+++ hora_escola/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from hora_escola.core import urls as core_urls
 
 urlpatterns = [
+    path('', include(core_urls)),
     path('admin/', admin.site.urls),
 ]
