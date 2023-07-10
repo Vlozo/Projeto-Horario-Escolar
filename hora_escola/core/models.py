9358856f1148d39ad9b1ca0a8e60fe360dffa8c6
Welington Carlos <wcarlos3@gmail.com>
2023-07-10 14:35:50 -0300

    Alterações feitas

--- hora_escola/core/models.py
+++ hora_escola/core/models.py
@@ -1,11 +1,14 @@
 from django.db import models
-#possivelmente instalar django-phone-field para validação de telefone
+
+
+# possivelmente instalar django-phone-field para validação de telefone
 
 class Turma(models.Model):
     turma = models.IntegerField(null=True)
 
     def __str__(self):
         return f"{self.turma}"
+
 
 class Aluno(models.Model):
     nome = models.CharField(max_length=100)
@@ -15,6 +18,7 @@ class Aluno(models.Model):
 
     def __str__(self):
         return f"{self.nome} {self.turma}"
+
 
 class Responsavel(models.Model):
     nome = models.CharField(max_length=100)
@@ -29,12 +33,9 @@ class Responsavel(models.Model):
         verbose_name = "Responsável"
         verbose_name_plural = "Responsáveis"
 
+
 class Club(models.Model):
     nome = models.CharField(max_length=100)
+
     def __str__(self):
         return f"{self.nome}"
-
-
-
-
-# Create your models here.
