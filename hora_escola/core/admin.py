1aebb2976fcf26d9a4dacf4d1d23a99403b632a5
Vlozo <alevelozo26@gmail.com>
2023-10-19 19:49:40 -0300

    Novos campos adicionados ao Admin-View

--- hora_escola/core/admin.py
+++ hora_escola/core/admin.py
@@ -1,29 +1,15 @@
-# from django.contrib import admin
-# from .models import *
-#
-# class TurmaAdmin(admin.ModelAdmin):
-#     list_display = ("turma",)
-#
-# class AlunoAdmin(admin.ModelAdmin):
-#     list_display = ("nome", "turma",)
-#     list_filter = ("turma",)
-#     search_fields = ("nome",)
-#
-# class ResponsavelAdmin(admin.ModelAdmin):
-#     list_display = ("nome", "telefone", "dependente",)
-#     search_fields = ("nome",)
-#
-# class ClubAdmin(admin.ModelAdmin):
-#     list_display = ("nome",)
-#     search_fields = ("nome",)
-#
-# class UsuarioAdmin(admin.ModelAdmin):
-#     readonly_fields = ('matricula', 'data_nascimento', 'email', 'telefone', 'senha')
-#
-# admin.site.register(Turma, TurmaAdmin)
-# admin.site.register(Aluno, AlunoAdmin)
-# admin.site.register(Responsavel, ResponsavelAdmin)
-# admin.site.register(Club, ClubAdmin)
-# admin.site.register(Usuario, UsuarioAdmin)
-#
-# # Register your models here.
+from django.contrib import admin
+from .models import Client, Room_Class
+
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('user', 'type', 'room_class', 'student_id')
+    list_filter = ('room_class', 'type')
+    search_fields = ('student_id', 'user')
+
+class Room_ClassAdmin(admin.ModelAdmin):
+    list_display = ("room_class",)
+    
+admin.site.register(Client, ClientAdmin)
+admin.site.register(Room_Class, Room_ClassAdmin)
+
+
